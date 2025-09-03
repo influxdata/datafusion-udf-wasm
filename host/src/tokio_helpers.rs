09280041d2daa2fe40043523dace5fa693a20db5
Marco Neumann <marco@crepererum.net>
2025-09-03 12:55:33 +0200

    docs: enforce documentation for private items

--- host/src/tokio_helpers.rs
+++ host/src/tokio_helpers.rs
@@ -1,5 +1,10 @@
-// this is a hack that is required because the respective DataFusion interfaces aren't fully async
-// TODO: remove this!
+//! Helpers to simplify work with [`tokio`].
+
+/// Run an async method in a sync context.
+///
+/// **This is a hack that is required because the respective DataFusion interfaces aren't fully async.**
+///
+/// TODO: remove this! See <https://github.com/influxdata/datafusion-udf-wasm/issues/31>.
 pub(crate) fn async_in_sync_context<Fut>(fut: Fut) -> Fut::Output
 where
     Fut: Future,
@@ -7,6 +12,7 @@ where
     tokio::task::block_in_place(move || tokio::runtime::Handle::current().block_on(fut))
 }
 
+/// Spawn blocking I/O.
 pub(crate) async fn blocking_io<F, T>(f: F) -> std::io::Result<T>
 where
     F: FnOnce() -> std::io::Result<T> + Send + 'static,
