07135afaf6b0d649b802f57126753960be6470e6
Marco Neumann <marco@crepererum.net>
2025-11-27 16:58:40 +0100

    ci: extract "free disk space" action
    We likely need this for the release workflow as well.
    
    Ref #186.

---
+++ .github/actions/free-disk-space/free.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+df -h
+
+to_delete=(
+  "/lib/firefox"
+  "/lib/google-cloud-sdk"
+  "/lib/jvm"
+  "/lib/llvm-16"
+  "/lib/llvm-17"
+  "/opt/az"
+  "/opt/google"
+  "/opt/hostedtoolcache/CodeQL"
+  "/opt/hostedtoolcache/PyPy"
+  "/opt/hostedtoolcache/Ruby"
+  "/opt/hostedtoolcache/go"
+  "/opt/hostedtoolcache/node"
+  "/opt/microsoft"
+  "/opt/pipx"
+  "/usr/share/swift"
+)
+for d in "${to_delete[@]}"; do
+  echo "delete $d"
+  sudo rm -rf "$d"
+done
+
+df -h
